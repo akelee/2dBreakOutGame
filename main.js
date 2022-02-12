11746d1e0876330f99f2fa9b8f61b5c13dc4a4f1
Alex <80860409+akelee@users.noreply.github.com>
2022-02-12 20:59:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,26 +9,40 @@ class Block {
         this.bottomLeft =  [xAxis, yAxis];//blocks anchor point
         this.bottomRight = [xAxis + blockWidth, yAxis];
         this.topLeft = [xAxis, yAxis + blockHeight];
-        this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
     }
 }
 
 //All blocks
 
 const blocks = [
-    new Block(10, 270);
-]
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
+  ]
 
 //Draw my block
 
+function addBlocks() {
 for (let i = 0; i < blocks.length; i++) {
-    function addBlocks() {
-const block = document.createElement('div');
-block.classList.add('block');//give block of classlist block
-block.style.left = blocks[i].bottomLeft[0]; //get x-axis & assign to left at index 0 position
-block.style.bottom = '50px';
-grid.appendChild(block)//put style of block onto grid
-    }
+    const block = document.createElement('div');
+    block.classList.add('block');//give block of classlist block
+    block.style.left = blocks[i].bottomLeft[0] + 'px'; //get x-axis & assign to left at index 0 position
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px';
+    grid.appendChild(block)//put style of block onto grid
+    console.log(blocks[i].bottomLeft);
+        }
 }
-
 addBlocks();
